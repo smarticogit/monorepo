e87e6e79ac7dd842554a5e9f9296738663913b88
Daniel <daniel.repres@hotmail.com>
2024-09-07 14:02:42 -0300

    feat: setting environment variables

--- apps/user/src/app.module.ts
+++ apps/user/src/app.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { UserCreateController } from './controllers/user-create.controller';
 import { UserFindAllController } from './controllers/user-find-all.controller';
+import { envSchema } from './env';
 import { PrismaService } from './prisma/prisma.service';
 
 @Module({
+  imports: [
+    ConfigModule.forRoot({
+      validate: (env) => envSchema.parse(env),
+      isGlobal: true,
+    }),
+  ],
   controllers: [UserCreateController, UserFindAllController],
   providers: [PrismaService],
 })
--- apps/user/src/main.ts
+++ apps/user/src/main.ts
@@ -1,8 +1,14 @@
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Env } from './env';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  const configServeice = app.get<ConfigService<Env, true>>(ConfigService);
+  const port = configServeice.get('PORT', { infer: true });
+
+  await app.listen(port);
 }
 bootstrap();
